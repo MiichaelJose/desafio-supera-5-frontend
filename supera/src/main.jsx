3e48eaaa758c35e3b8adfe66156a1a93e9f91fe2
MiichaelJose <michaeljosecraft1@gmail.com>
2022-12-25 13:48:53 -0300

    alteracao pages

--- supera/src/main.jsx
+++ supera/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Pages/Tela';
+import App from './pages/Tela';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
